3cd5984dd75ec55416260c275d95934a87614976
sumin <yming3595@gmail.com>
2024-10-14 20:40:42 +0900

    디자인: header, footer 생성 및 타임리프로 가져오기

--- src/main/java/com/example/tnj/TnjApplication.java
+++ src/main/java/com/example/tnj/TnjApplication.java
@@ -3,10 +3,12 @@ package com.example.tnj;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 
-@SpringBootApplication
-@ComponentScan
+@SpringBootApplication(exclude= DataSourceAutoConfiguration.class)
+@ComponentScan(basePackages = {"com.example.tnj","mybatis.dao"})
 @MapperScan
 public class TnjApplication {
 
