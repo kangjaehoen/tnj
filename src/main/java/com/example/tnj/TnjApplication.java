4a3843997c9dc114ebfdc382973b1fba1a252309
Kyung_A <audwk6028@naver.com>
2024-10-18 13:49:13 +0900

    회원가입 구현

--- src/main/java/com/example/tnj/TnjApplication.java
+++ src/main/java/com/example/tnj/TnjApplication.java
@@ -3,11 +3,12 @@ package com.example.tnj;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
-@ComponentScan
-@MapperScan
+@SpringBootApplication//(exclude= DataSourceAutoConfiguration.class)
+@ComponentScan(basePackages = {"com.example.tnj","mybatis.dao"})
+@MapperScan(value = {"mybatis.dao"})
 public class TnjApplication {
 
 	public static void main(String[] args) {
