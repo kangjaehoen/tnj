36c16fc4a6a0f93edbb15d1a27d4291314b88ffd
sumin <yming3595@gmail.com>
2024-10-21 15:46:11 +0900

    결제테이블 resNum insert

--- src/main/java/com/example/tnj/Controller/PaymentController.java
+++ src/main/java/com/example/tnj/Controller/PaymentController.java
@@ -28,9 +28,9 @@ public class PaymentController {
     public String insertPay(@RequestBody PaymentDTO pDTO, @ModelAttribute ReservationDTO rDTO,
                             @RequestParam("accomNum") int accomNum){
         try{
-//            int resNum= dao.selectresNum(String.valueOf(rDTO.getResNum()));
+            int resNum= dao.selectresNum(accomNum);
 
-            int resNum = rDTO.getResNum();
+//            int resNum = rDTO.getResNum();
             pDTO.setResNum(resNum);
             pDTO.setAccomNum(accomNum);
             System.out.println("Received PaymentDTO: " + pDTO);
