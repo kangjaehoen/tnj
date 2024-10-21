132c7536dddbe3611e14abf71da5e8d6789c5f1e
sumin <yming3595@gmail.com>
2024-10-21 15:40:49 +0900

    결제테이블 resNum insert

--- src/main/java/mybatis/dao/PaymentMapper.java
+++ src/main/java/mybatis/dao/PaymentMapper.java
@@ -8,7 +8,7 @@ import org.apache.ibatis.annotations.*;
 public interface PaymentMapper {
     //예약테이블에 생성된 resNum 가져오기
     @Select("select resNum from reservation where accomNum=#{accomNum} ORDER BY resNum DESC LIMIT 1")
-    public int selectresNum(String accomNum);
+    public int selectresNum(@Param("accomNum") int accomNum);
 
     //결제테이블 insert
     @Insert("insert into pay( resNum, id, payDate, pay_Status, payment, accomNum, amount, impUid, merchantUid, productName)  values( #{resNum},#{id}, now(), 'Y', #{payment}, #{accomNum}, #{amount}, #{impUid}, #{merchantUid}, #{productName})")
