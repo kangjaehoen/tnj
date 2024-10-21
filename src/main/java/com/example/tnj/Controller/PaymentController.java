8c817f77d5db4535d2fb5048d9a406179768fb1d
sumin <yming3595@gmail.com>
2024-10-21 21:15:20 +0900

    payment에 데이터 보여주기

--- src/main/java/com/example/tnj/Controller/PaymentController.java
+++ src/main/java/com/example/tnj/Controller/PaymentController.java
@@ -1,5 +1,6 @@
 package com.example.tnj.controller;
 
+import com.example.tnj.domain.AccVO;
 import com.example.tnj.domain.PaymentDTO;
 import com.example.tnj.domain.ResVO;
 import com.example.tnj.domain.ReservationDTO;
@@ -9,19 +10,29 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.beans.Transient;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class PaymentController {
     @Autowired
     PaymentMapper dao;
 
-    @GetMapping("/payment")
-    public String payment(){
+
+   @GetMapping("/payment")
+    public String payment(Model model, @ModelAttribute AccVO ac,@RequestParam("accomNum") int accomNum){
+
+       int resNum= dao.selectresNum(accomNum);
+       List<PaymentDTO> list= dao.listAll(resNum);
+       model.addAttribute("list",list);
+
         return "payment";
     }
+
 
 
    @PostMapping("/reservation/payment")
@@ -30,17 +41,22 @@ public class PaymentController {
         try{
             int resNum= dao.selectresNum(accomNum);
 
-//            int resNum = rDTO.getResNum();
             pDTO.setResNum(resNum);
             pDTO.setAccomNum(accomNum);
             System.out.println("Received PaymentDTO: " + pDTO);
             dao.insertPay(pDTO);
+
             return "payment";
         }catch (Exception e){
             e.printStackTrace();
             return "redirect:/";
         }
 
+
     }
+
+
+
+
 
 }
