2ecdd23c098475c64c6a8b89e51efcd93238fa14
joonho <junho9661@gmail.com>
2024-10-22 20:33:50 +0900

    결제 가능 날짜 등록 수정

--- src/main/java/com/example/tnj/controller/test.java
+++ src/main/java/com/example/tnj/controller/test.java
@@ -1,6 +1,7 @@
 package com.example.tnj.controller;
 
 import mybatis.dao.AbledateMapper;
+import mybatis.dao.Testdata;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,10 +12,20 @@ import java.time.LocalDate;
 public class test {
     @Autowired
     AbledateMapper abd;
+    @Autowired
+     Testdata td;
+
     @RequestMapping("/crdate")
     public String test(){
         for(int i = 0 ; i < 100 ; i++){
         abd.createdate(11, LocalDate.now().plusDays(i),"able");
+        }
+        return "forTest";
+    }
+    @RequestMapping("/cpdate")
+    public String test2(){
+        for(int i = 0 ; i < 10 ; i++){
+            td.insertpaydata(i+1000,"user"+i,LocalDate.now().plusDays(i),28,40000,"롯데월드");
         }
         return "forTest";
     }
